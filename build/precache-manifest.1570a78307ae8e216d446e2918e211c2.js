675a738bea09f9432f12d28bfea990474c019bb8
Maximilian Barth <m.barth@unibas.ch>
2023-03-30 23:22:29 +0200

    started playing around with react

---
+++ build/precache-manifest.1570a78307ae8e216d446e2918e211c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f55f5fd9975722b14f30",
+    "url": "/static/css/main.47676e73.chunk.css"
+  },
+  {
+    "revision": "f55f5fd9975722b14f30",
+    "url": "/static/js/main.f55f5fd9.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "bd5440dfbcfe3e971529",
+    "url": "/static/js/runtime~main.bd5440df.js"
+  },
+  {
+    "revision": "35e74174b6820828a33081361ea735e0",
+    "url": "/index.html"
+  }
+];
